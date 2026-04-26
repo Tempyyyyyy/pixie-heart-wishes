6c9cb3b58ba2dc503332a774ebad0c72661eb7cf
Tempyyyyyy <tempyyyyyy@users.noreply.github.com>
2026-04-26 22:19:08 +0300

    Улучшено определение окружения для сайта

--- src/lib/environment.ts
+++ src/lib/environment.ts
@@ -1,5 +1,9 @@
 export const isElectron = () => {
-  return typeof window !== 'undefined' && !!(window as any).electron;
+  return typeof window !== 'undefined' && (
+    !!(window as any).electron || 
+    !!(window as any).electronAPI ||
+    navigator.userAgent.toLowerCase().indexOf(' electron/') > -1
+  );
 };
 
 export const isWebsite = () => {
